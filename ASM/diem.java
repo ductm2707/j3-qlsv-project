8a96c8817516d6d43a4b387dccae7e82c08a7acc
ductm2707 <85263912+ductm2707@users.noreply.github.com>
2021-06-04 17:06:37 +0700

    j3

---
+++ ASM/diem.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ASM;
+
+/**
+ *
+ * @author Admin
+ */
+public class diem {
+    String masv;
+    String hoten;
+    int tienganh;
+    int tinhoc;
+    int gdtc;
+
+    public diem(String masv, String hoten, int tienganh, int tinhoc, int gdtc) {
+        this.masv = masv;
+        this.hoten = hoten;
+        this.tienganh = tienganh;
+        this.tinhoc = tinhoc;
+        this.gdtc = gdtc;
+    }
+    
+    
+}
+
---
+++ ASM/sinhvien.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ASM;
+
+/**
+ *
+ * @author Admin
+ */
+public class sinhvien {
+    String masv;
+    String hoten;
+    String email;
+    String sodt;
+    boolean gioitinh;
+    String diachi;
+    String hinh;
+
+    public sinhvien(String masv, String hoten, String email, String sodt, boolean gioitinh, String diachi, String hinh) {
+        this.masv = masv;
+        this.hoten = hoten;
+        this.email = email;
+        this.sodt = sodt;
+        this.gioitinh = gioitinh;
+        this.diachi = diachi;
+        this.hinh = hinh;
+    }
+    
+    
+}
